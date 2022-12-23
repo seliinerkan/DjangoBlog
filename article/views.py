79b5a65c320890c4980e6af3943c91d79cf6e157
seliinerkan <118636863+seliinerkan@users.noreply.github.com>
2022-12-23 14:59:18 +0300

    crispy ile sayfalar responsive

--- article/views.py
+++ article/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render ,HttpResponse
 
 # Create your views here.
+def index(request):
+    context = {
+      "number1":[1,2,3,4,5,6]
+      
+    }
+    return render(request, "index.html",context)
+
+def about(request):
+    return render(request,"about.html")
+
+
+
+
--- djangoBLog/settings.py
+++ djangoBLog/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "article",
+    "user",
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'djangoBLog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -106,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Istanbul'
 
 USE_I18N = True
 
@@ -119,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
