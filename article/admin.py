1d4ab180b0aef186a70492fddc3a6277b6804ef5
seliinerkan <118636863+seliinerkan@users.noreply.github.com>
2022-12-29 23:33:00 +0300

    Django App has completed

--- article/admin.py
+++ article/admin.py
@@ -1,15 +1,21 @@
 from django.contrib import admin
 
-from .models import Article
-# .models dosyamızın içinde oluturduğumuz Article modelini importladık çünkü oluşan modeli admin sayfasında görüntülemek istiyoruz
+from .models import Article,Comment
+
 # Register your models here.
+
+admin.site.register(Comment)
+
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
 
-    list_display = ["title","author", "created_date"]
-    list_display_links = ["title", "created_date"]
+    list_display = ["title","author","created_date"]
+
+    list_display_links = ["title","created_date"]
+
     search_fields = ["title"]
+
     list_filter = ["created_date"]
     class Meta:
-      model = Article
+        model = Article
 
--- user/urls.py
+++ user/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from . import views
 
 app_name = "user"
+
 urlpatterns = [
-    path('register/', views.register, name = "register"),
-    path('login/', views.loginUser, name = "login"),
-    path('logout/', views.logoutUser, name = "logout"),
-    
+    path('register/',views.register,name = "register"),
+    path('login/',views.loginUser,name = "login"),
+    path('logout/',views.logoutUser,name = "logout"),
+
 ]
